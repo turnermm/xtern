0872af344a505c2890f5d9e77ced2c5efdea2766
Myron Turner <turnermm02@shaw.ca>
2019-02-17 07:44:41 -0600

    Conceal (#3)
    * Conceal broken link markup except when im preview
    
    * Implements setting to conceal display broken link markup except when in preview mode

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,3 +1,5 @@
 <?php
 $meta['ca_required'] = array('onoff');
 $meta['max_time'] = array('numeric');
+$meta['conceal']  = array('onoff');
+
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,3 +1,4 @@
 <?php
 $lang['ca_required'] = 'This wiki uses a cacerts.pem file installed in the <code>xtern/ca</code> directory.';
 $lang['max_time'] = 'Time in seconds to allow for broken link checking in the admin panel.  Defaults to 120';
+$lang['conceal'] = 'Display <code>BROKEN-LINK</code> markup in preview mode only; hide it for general viewing of pages.';
